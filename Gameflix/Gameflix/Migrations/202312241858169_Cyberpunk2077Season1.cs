e751415edf2a5068197e26c3dd9cf9290d2f7004
Eric Tashji <99293073+etashji@users.noreply.github.com>
2023-12-24 14:05:26 -0500

    Completed Cyberpunk 2077 Season 1

---
+++ Gameflix/Gameflix/Migrations/202312241858169_Cyberpunk2077Season1.cs
@@ -0,0 +1,29 @@
+﻿namespace Gameflix.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Cyberpunk2077Season1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Season1",
+                c => new
+                    {
+                        Season1Id = c.Int(nullable: false, identity: true),
+                        episodeNumber = c.Int(nullable: false),
+                        Title = c.String(nullable: false),
+                        Description = c.String(nullable: false),
+                        Video = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Season1Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Season1");
+        }
+    }
+}
